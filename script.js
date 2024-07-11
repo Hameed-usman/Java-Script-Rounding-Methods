82ededd1cba280a21de7f388d00f0ebdf2db2452
Hameed-usman <usman.hameed1145@gmail.com>
2024-07-11 17:50:55 +0500

    almost

---
+++ script.js
@@ -0,0 +1,35 @@
+                    //   ==================TASK 01=================
+
+// let userinput = prompt("Enter A String");
+
+// let index = prompt("Enter an index");
+
+// let character = userinput.charAt(index);
+
+// if (index >= 0 && index < userinput.length) {
+//     alert("The character at index " + index + " is '" + character + " ");
+// } else {
+//     alert("Invalid!!");
+// }
+
+
+                     // =====================TASK 02==================
+
+// let userNum = prompt("Enter A Number in Float");
+// let roundNum = (Math.round(userNum));
+// alert ("Round Number is :" +roundNum );
+
+                       // =============  TASK  03  ==================
+
+// let userNum = prompt ("Enter A Number ");
+// let ceilNum = (Math.ceil(userNum));
+// alert ("Ceil Number is : " +ceilNum);
+
+
+// =============   TASK  04   =================
+
+// let userNum = prompt ("Enter A Number ");
+// let floorNum = (Math.floor(userNum));
+// alert ("Floor Number is : " +floorNum);
+
+
